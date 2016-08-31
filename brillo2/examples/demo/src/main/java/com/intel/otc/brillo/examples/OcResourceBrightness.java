396609d523400d4aab6fd7295e5b72c221ca8e94
Tonny Tzeng <tonny.tzeng@intel.com>
2016-08-31 12:12:25 +0800

    Move OcPlatform.EntityHandler interface instance to OcResourceBase abstract class

--- brillo2/examples/demo/src/main/java/com/intel/otc/brillo/examples/OcResourceBrightness.java
+++ brillo2/examples/demo/src/main/java/com/intel/otc/brillo/examples/OcResourceBrightness.java
@@ -6,22 +6,17 @@ import org.iotivity.base.EntityHandlerResult;
 import org.iotivity.base.OcException;
 import org.iotivity.base.OcPlatform;
 import org.iotivity.base.OcRepresentation;
-import org.iotivity.base.OcResourceHandle;
 import org.iotivity.base.OcResourceRequest;
 import org.iotivity.base.OcResourceResponse;
-import org.iotivity.base.RequestHandlerFlag;
-import org.iotivity.base.RequestType;
 import org.iotivity.base.ResourceProperty;
 
 import java.util.EnumSet;
 
-public class OcResourceBrightness implements OcPlatform.EntityHandler {
+public class OcResourceBrightness extends OcResourceBase {
     private static final String TAG = OcResourceBrightness.class.getSimpleName();
     public  static final String RESOURCE_TYPE = "oic.r.light.brightness";
     private static final String KEY_BRIGHTNESS = "brightness";
-    private static int SUCCESS = 200;
 
-    private OcResourceHandle mHandle;
     private int brightness;
     private OnBrightnessChangeListener brightnessChangeListener = null;
 
@@ -42,53 +37,7 @@ public class OcResourceBrightness implements OcPlatform.EntityHandler {
         }
     }
 
-    public synchronized void destroy() {
-        if (null != mHandle) {
-            try {
-                OcPlatform.unregisterResource(mHandle);
-                Log.d(TAG, "Resource unregistered");
-            } catch (OcException e) {
-                error(e, "Failed to unregister resource");
-            }
-            mHandle = null;
-        }
-    }
-
-    @Override
-    public synchronized EntityHandlerResult handleEntity(final OcResourceRequest request) {
-        EntityHandlerResult result = EntityHandlerResult.ERROR;
-        if (null != request && null != mHandle) {
-            // Get the request flags
-            EnumSet<RequestHandlerFlag> requestFlags = request.getRequestHandlerFlagSet();
-            if (requestFlags.contains(RequestHandlerFlag.INIT)) {
-                Log.d(TAG, "\tRequest Flag: Init");
-                result = EntityHandlerResult.OK;
-            }
-            if (requestFlags.contains(RequestHandlerFlag.REQUEST)) {
-                Log.d(TAG, "\tRequest Flag: Request");
-                result = handleRequest(request);
-            }
-            if (requestFlags.contains(RequestHandlerFlag.OBSERVER)) {
-                Log.e(TAG, "\tRequest Flag: Observer - Not implemented yet");
-            }
-        }
-        return result;
-    }
-
-    private EntityHandlerResult handleRequest(final OcResourceRequest request) {
-        EntityHandlerResult result = EntityHandlerResult.ERROR;
-        RequestType requestType = request.getRequestType();
-        switch (requestType) {
-            case GET:
-                result = handleGetRequest(request);
-                break;
-            case POST:
-                result = handlePostRequest(request);
-        }
-        return result;
-    }
-
-    private EntityHandlerResult handleGetRequest(final OcResourceRequest request) {
+    protected EntityHandlerResult handleGetRequest(OcResourceRequest request) {
         OcResourceResponse response = new OcResourceResponse();
         response.setRequestHandle(request.getRequestHandle());
         response.setResourceHandle(request.getResourceHandle());
@@ -97,34 +46,14 @@ public class OcResourceBrightness implements OcPlatform.EntityHandler {
             public void run() {
                 response.setErrorCode(SUCCESS);
                 response.setResponseResult(EntityHandlerResult.OK);
-                response.setResourceRepresentation(getRepresentation());
+                response.setResourceRepresentation(getOcRepresentation());
                 sendResponse(response);
             }
         }).start();
         return EntityHandlerResult.SLOW;
     }
 
-    private EntityHandlerResult handlePostRequest(final OcResourceRequest request) {
-        OcResourceResponse response = new OcResourceResponse();
-        response.setRequestHandle(request.getRequestHandle());
-        response.setResourceHandle(request.getResourceHandle());
-        setRepresentation(request.getResourceRepresentation());
-        response.setErrorCode(SUCCESS);
-        return sendResponse(response);
-    }
-
-    private EntityHandlerResult sendResponse(OcResourceResponse response) {
-        EntityHandlerResult result = EntityHandlerResult.ERROR;
-        try {
-            OcPlatform.sendResponse(response);
-            result = EntityHandlerResult.OK;
-        } catch (OcException e) {
-            error(e, "Failed to send response");
-        }
-        return result;
-    }
-
-    public OcRepresentation getRepresentation() {
+    protected OcRepresentation getOcRepresentation() {
         OcRepresentation rep = null;
         if (null != mHandle) {
             rep = new OcRepresentation();
@@ -137,7 +66,7 @@ public class OcResourceBrightness implements OcPlatform.EntityHandler {
         return rep;
     }
 
-    public void setRepresentation(OcRepresentation rep) {
+    protected void setOcRepresentation(OcRepresentation rep) {
         if (null != mHandle) {
             try {
                 if (rep.hasAttribute(KEY_BRIGHTNESS)) brightness = rep.getValue(KEY_BRIGHTNESS);
@@ -148,9 +77,5 @@ public class OcResourceBrightness implements OcPlatform.EntityHandler {
                 error(e, "Failed to get '" + KEY_BRIGHTNESS + "' representation value");
             }
         }
-    }
-
-    private void error(OcException e, String msg) {
-        Log.e(TAG, msg + "\n" + e.toString());
     }
 }
