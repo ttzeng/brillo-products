2ce3775bbbe5f97b36e852c913b77444ef80041c
Tonny Tzeng <tonny.tzeng@intel.com>
2017-01-12 16:45:57 +0800

    Resources manage the resource interface and property set during the registration

--- brillo2/examples/demo/src/main/java/com/intel/otc/brillo/examples/MainActivity.java
+++ brillo2/examples/demo/src/main/java/com/intel/otc/brillo/examples/MainActivity.java
@@ -4,11 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-import org.iotivity.base.OcPlatform;
-import org.iotivity.base.ResourceProperty;
-
-import java.util.EnumSet;
-
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -36,24 +31,16 @@ public class MainActivity extends Activity {
         ocServer = new OcServer(this);
         ocBrightness = new OcResourceBrightness(
                 "/brillo/mp3player/brightness",
-                OcPlatform.DEFAULT_INTERFACE,
-                EnumSet.of(ResourceProperty.DISCOVERABLE, ResourceProperty.OBSERVABLE),
                 100, lcdDisplayManager);
         ocColorRGB = new OcResourceColorRGB(
                 "/brillo/mp3player/rgb",
-                OcPlatform.DEFAULT_INTERFACE,
-                EnumSet.of(ResourceProperty.DISCOVERABLE, ResourceProperty.OBSERVABLE),
                 lcdDisplayManager);
         ocMp3Player = new OcResourceMp3Player(
                 "/brillo/mp3player",
-                OcPlatform.DEFAULT_INTERFACE,
-                EnumSet.of(ResourceProperty.DISCOVERABLE, ResourceProperty.OBSERVABLE),
                 mp3Player);
         mp3Player.subscribeStateChangeNotification(ocMp3Player);
         ocAudioControl = new OcResourceAudioControl(
                 "/brillo/mp3player/volume",
-                OcPlatform.DEFAULT_INTERFACE,
-                EnumSet.of(ResourceProperty.DISCOVERABLE, ResourceProperty.OBSERVABLE),
                 mp3Player);
     }
 
